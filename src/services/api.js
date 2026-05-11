df369f17d161eee59b0d8c1d46b768f86c76d0ed
om31104 <om.bukkawar23@vit.edu>
2026-05-11 09:49:52 +0530

    LOCALHOST CHANGE

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import.meta.env.VITE_API_URL
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Initialize Axios instance
 const api = axios.create({
